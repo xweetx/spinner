26d25fdb6fbec78a0b9260a1fef7bc7c01508582
xweetx <xweet_xilenze@yahoo.com>
2023-06-11 15:08:27 -0600

    spinner1

---
+++ spinner1.js
@@ -0,0 +1,35 @@
+const spinner = () => {
+  setTimeout(()=>{
+    process.stdout.write('\r|   ')
+  }, 100)
+  setTimeout(()=>{
+    process.stdout.write("\r/   ")
+  }, 300)
+  setTimeout(()=>{
+    process.stdout.write("\r-   ")
+  }, 500)
+  setTimeout(()=>{
+    process.stdout.write("\r\\   ")
+  }, 700)
+  setTimeout(()=>{
+    process.stdout.write('\r|   ')
+  }, 900)
+  setTimeout(()=>{
+    process.stdout.write("\r/   ")
+  }, 1100)
+  setTimeout(()=>{
+    process.stdout.write("\r-   ")
+  }, 1300)
+  setTimeout(()=>{
+    process.stdout.write("\r\\   ")
+  }, 1500)
+  setTimeout(()=>{
+    process.stdout.write("\r|   ")
+  }, 1700)
+  setTimeout(() => {
+    process.stdout.write("\n");
+  }, 1900);
+}
+  
+
+  spinner()
